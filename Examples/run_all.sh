4db01f02c6dfd6893556786e5faf21d30f8775a4
Tim Massingham <tim.massingham@gmail.com>
2016-06-20 21:39:35 +0100

    Fix a seed and make output files configurable

--- Examples/run_all.sh
+++ Examples/run_all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 SLR=`realpath -e ../bin/Slr`
+prefix=$1
 NPARALLEL=1
 
 ls -d -p * | grep '/$' | xargs -n 1 -I %% -P ${NPARALLEL} sh -c "
@@ -7,6 +8,6 @@ ls -d -p * | grep '/$' | xargs -n 1 -I %% -P ${NPARALLEL} sh -c "
 	cd %%
 	if [ ! -e 'SKIP_RUN' ]
 	then 
-	    OPENBLAS_NUM_THREADS=1 time -p stdbuf -o 0 ${SLR} -skipsitewise 1 > run2.log
+	    OPENBLAS_NUM_THREADS=1 time -p stdbuf -o 0 ${SLR} -seed 123 -skipsitewise 1 > ${prefix}.log
 	fi
 "
